e18cbb9fe1a980e10937e9a15e514124b3b3f0c3
raa <alexander.thequeen@gmail.com>
2024-12-17 18:03:59 +0300

    saving

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bw_distance
 alg for testing: generate distance to bw
+
+## todo
+- [ ] add logging
+- [ ] check what is pwd during real invoking
+- [ ] add required cli args for grid dimensions
+- [ ] add properties3d
+- [ ] add 'real' algorithm
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+
+fn main() -> Result<()> {
+    println!("parser intro");
+    let args = CliArgs::parse();
+    println!("{} -> {}", args.commandName, args.value);
+    println!("{} -> {}", args.uflA, args.wflB);
+
+    Ok(())
+}
+
+//  //  //  //  //  //  //  //
+#[derive(Parser, Debug)]
+#[command(about)]
+struct CliArgs {
+    #[arg(short, long, default_value_t = false)]
+    uflA: bool,
+    #[arg(short, long, default_value_t = false)]
+    wflB: bool,
+    #[arg(short, long)]
+    commandName: String,
+    #[arg(short, long, default_value_t = 0)]
+    value: u8,
 }
